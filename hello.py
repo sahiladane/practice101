ca75aab8f495dab037fe2743c0b1fe6eb703bf42
sahiladane <sahiladane@gmail.com>
2024-03-14 15:30:30 -0500

    test2 changes

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-hello
-this is added in the test2 branch
+import pandas as pd
+
+dict1 = { 'a': [1, 3, 5, 7], 'b': [4, 5, 7, 0]}
+
+pd.DataFrame(dict1)
+
+#pd.iloc[0]
+pd.iloc[1]
+
+pd.add(pd.iloc[0], axis = 0)
+
+
+pandas.sum()
